af0feb227ad65a8fc4bac68ad2c4be1e802d2229
jkvim <jkvim@outlook.com>
2018-09-22 22:28:09 +0800

    feature: add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
-import { AppController } from './app.controller'
-import { CatsController } from 'cats/cat.controller'
-import { CatService } from 'cats/cat.service'
 import { CatModule } from './cats/cat.module'
-import { AppService } from 'app.service'
+import { UsersModule } from './users/users.module'
 import { join } from 'path'
 
 @Module({
   imports: [
     CatModule,
+    UsersModule,
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       installSubscriptionHandlers: true,
